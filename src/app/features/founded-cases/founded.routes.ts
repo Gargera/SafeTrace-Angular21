2361693ad666fb85331f13e6fd4c2661ea90ad61
Mohamed Saeed <muhameds913@gmail.com>
2026-06-29 13:21:24 +0300

    update

--- src/app/features/founded-cases/founded.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { FoundedListComponent } from './pages/founded-page.component';
-
-export const FOUNDED_ROUTES: Routes = [
-  {
-    path: '',
-    component: FoundedListComponent,
-  },
-];
