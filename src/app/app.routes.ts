cade32fa5da61bd9f4eae5276a4341eec5b2be4c
Gargera <et93512@gmail.com>
2026-07-06 03:46:48 +0300

    add logo and fonts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { adminGuard } from './core/guards/admin-guard';
 import { About } from './shared/components/about/about';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full', title: 'الرئيسية | لقاء' },
+
   { 
     path: 'auth', 
     loadChildren: () => import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES) 
@@ -18,52 +20,56 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./shared/components/main-layout/main-layout').then(c => c.MainLayout),
     children: [
+      {
+        path: '', 
+        title: "الرئيسية | لقاء",
+        loadChildren: () => import('./features/home-support/home-support.routes').then(m => m.HOME_SUPPORT_ROUTES) 
+      },
       { 
         path: 'home', 
-        title: "الرئيسية",
+        title: "الرئيسية | لقاء",
         loadChildren: () => import('./features/home-support/home-support.routes').then(m => m.HOME_SUPPORT_ROUTES) 
       },
       { 
         path: 'about', 
-        component: About,
-        title: "دليل المنصة"
+        title: "دليل المنصة | لقاء",
+        component: About
       },
       { 
         path: 'founded', 
-        title: "الحالات المعثور عليها",
+        title: "الحالات المعثور عليها | لقاء",
         loadChildren: () => import('./features/founded-cases/founded.routes').then(m => m.FOUNDED_ROUTES) 
       },
       { 
         path: 'urgent', 
-        title: "الحالات الطارئة",
+        title: "الحالات الطارئة | لقاء",
         loadChildren: () => import('./features/urgent-cases/urgent.routes').then(m => m.URGENT_ROUTES) 
       },
       { 
         path: 'long-term', 
-        title: "الحالات طويلة المدى",
+        title: "الحالات طويلة المدى | لقاء",
         loadChildren: () => import('./features/long-term-cases/long-term.routes').then(m => m.LONG_TERM_ROUTES) 
       },
       { 
         path: 'unknown', 
-        title: "الحالات الغير معروفة",
+        title: "الحالات الغير معروفة | لقاء",
         loadChildren: () => import('./features/unknown-cases/unknown.routes').then(m => m.UNKNOWN_ROUTES) 
       },
 
       { 
         path: 'aisearch', 
-        title: "البحث الذكي",
+        title: "البحث الذكي | لقاء",
         loadChildren: () => import('./features/ai-search/ai.routes').then(m => m.AiSearch_ROUTES) 
       },
       { 
         path: 'profile', 
-        title: "الملف الشخصي",
+        title: "الملف الشخصي | لقاء",
         canActivate: [authGuard],
         loadChildren: () => import('./features/user-profile/profile.routes').then(m => m.PROFILE_ROUTES) 
       }
     ]
   },
   
-  { path: '', redirectTo: 'home', pathMatch: 'full', title: "الرئيسية"},
   { path: '403', loadComponent: () => import('./shared/components/access-denied/access-denied').then(c => c.AccessDenied) },
   { path: '**', loadComponent: () => import('./shared/components/not-found/not-found').then(c => c.NotFound) }
 ];
