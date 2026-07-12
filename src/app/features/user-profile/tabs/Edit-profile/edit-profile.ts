7cd76d4ef3c5c90770278db1baa57d49b2e9eef5
Youssef farouk <faroukyousef0@gmail.com>
2026-07-12 17:54:47 +0300

    fix folder error

--- src/app/features/user-profile/tabs/Edit-profile/edit-profile.ts
+++ src/app/features/user-profile/tabs/Edit-profile/edit-profile.ts
@@ -34,8 +34,8 @@ import { ProfileService } from '../../service/profile.service';
 import { UserRole } from '../../../../shared/enums/user-role';
 import { VerificationStatus } from '../../../../shared/enums/verification-status';
 import { SnackbarService } from '../../../../core/services/toast.service';
-import { ConfirmDialog } from '../../Shared/confirm-dialog/confirm-dialog';
-import { ImageCropDialog } from '../../Shared/image-crop-dialog/image-crop-dialog';
+import { ConfirmDialog } from '../../shared/confirm-dialog/confirm-dialog';
+import { ImageCropDialog } from '../../shared/image-crop-dialog/image-crop-dialog';
 import { Toast } from '../../../../shared/components/toast/toast';
 
 // ── Egypt center coordinates (default) ────────────────────────────────────
