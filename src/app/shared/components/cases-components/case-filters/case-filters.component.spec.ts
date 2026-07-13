00e53c7e81aa89a2270f27ac289000ffe61bf4d8
Yousef721 <yousarsefabedalla255@gmail.com>
2026-07-13 15:30:26 +0300

    Update

--- src/app/shared/components/cases-components/case-filters/case-filters.component.spec.ts
+++ src/app/shared/components/cases-components/case-filters/case-filters.component.spec.ts
@@ -19,28 +19,4 @@ describe('CaseFiltersComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  it('should emit filterChange automatically when a filter value changes', fakeAsync(() => {
-    const emitSpy = jasmine.createSpy('filterChange');
-    component.filterChange.subscribe(emitSpy);
-
-    component.filterForm.get('gender')?.setValue(1);
-
-    tick();
-
-    expect(emitSpy).toHaveBeenCalled();
-  }));
-
-  it('should debounce fullName changes before emitting', fakeAsync(() => {
-    const emitSpy = jasmine.createSpy('filterChange');
-    component.filterChange.subscribe(emitSpy);
-
-    component.filterForm.get('fullName')?.setValue('Ali');
-
-    expect(emitSpy).not.toHaveBeenCalled();
-
-    tick(400);
-
-    expect(emitSpy).toHaveBeenCalledTimes(1);
-  }));
 });
