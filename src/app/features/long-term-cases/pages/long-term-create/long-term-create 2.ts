d17760d7da291287586d322329ffcbf90aba5a4e
Yousef721 <yousarsefabedalla255@gmail.com>
2026-07-15 19:12:24 +0300

    Update

--- src/app/features/long-term-cases/pages/long-term-create/long-term-create 2.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-long-term-create',
-  imports: [],
-  templateUrl: './long-term-create.html',
-  styleUrl: './long-term-create.css',
-})
-export class LongTermCreate {}
