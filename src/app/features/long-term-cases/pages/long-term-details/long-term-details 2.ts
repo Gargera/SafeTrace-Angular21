84ada964d5454fc285b917f98608c3646ac67735
Yousef721 <yousarsefabedalla255@gmail.com>
2026-07-15 21:31:37 +0300

    Update

--- src/app/features/long-term-cases/pages/long-term-details/long-term-details 2.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-long-term-details',
-  imports: [],
-  templateUrl: './long-term-details.html',
-  styleUrl: './long-term-details.css',
-})
-export class LongTermDetails {}
