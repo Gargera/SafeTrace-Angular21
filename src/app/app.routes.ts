b45f5380ca547cf3bca3547767b9d6bd134f1bc7
Gargera <et93512@gmail.com>
2026-07-16 23:20:47 +0300

    fix home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,6 @@ import { UserRole } from './shared/enums/user-role';
 import { Home } from './shared/components/home/home';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full', title: 'الرئيسية | لقاء' },
-
   { 
     path: 'auth', 
     loadChildren: () => import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES) 
